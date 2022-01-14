42b9b1788356a9c04468fde526047e384e8af379
Lucas Marchiori <sinonsrt@hotmail.com>
2022-01-13 23:09:26 -0300

    start to create services unit test

--- src/todo/todo.service.spec.ts
+++ src/todo/todo.service.spec.ts
@@ -1,18 +1,43 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 import { TodoService } from './todo.service';
+import { Todo } from './entities/todo.entity';
 
+const todoEntityList = [
+  new Todo({ task: 'task-1', is_done: 0, deleted_at: null }),
+  new Todo({ task: 'task-1', is_done: 0, deleted_at: null }),
+  new Todo({ task: 'task-1', is_done: 0, deleted_at: null }),
+];
 describe('TodoService', () => {
-  let service: TodoService;
+  let todoService: TodoService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TodoService],
+      providers: [
+        TodoService,
+        {
+          provide: getModelToken('Todo'),
+          useValue: {
+            find: jest.fn().mockResolvedValue(todoEntityList),
+            findById: jest.fn(),
+            findByIdAndUpdate: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<TodoService>(TodoService);
+    todoService = module.get<TodoService>(TodoService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(todoService).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return a todo list', async () => {
+      const result = await todoService.findAll();
+
+      expect(result).toEqual(todoEntityList);
+    });
   });
 });
